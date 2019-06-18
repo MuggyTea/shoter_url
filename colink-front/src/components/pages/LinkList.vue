d4a4875d660020db38a784421e3503b0579562a4
nf-mugicha <natsuki.fujimoto.kk@gmail.com>
2019-06-18 18:09:45 +0900

    add one Link photo component

--- colink-front/src/components/pages/LinkList.vue
+++ colink-front/src/components/pages/LinkList.vue
@@ -1,5 +1,27 @@
 <template>
-<h1>
-    LinkLists
-</h1>
+    <div class="photo-list">
+        <h1>Link List</h1>
+        <div class="grid">
+            <LinkPhoto
+                class="grid__item"
+                v-for="photo in photos"
+                :key="photo.id"
+                :item="photo"
+            />
+        </div>
+    </div>
 </template>
+
+<script>
+import LinkPhoto from './LinkPhoto'
+export default {
+  components: {
+    LinkPhoto
+  },
+  data () {
+    return {
+      photos: []
+    }
+  }
+}
+</script>
---
+++ colink-front/src/components/pages/LinkPhoto.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="photo">
+        <figure class="photo__wrapper">
+            <img
+                class="photo__image photo__image--portrait"
+                :src="item.url"
+                :alt="'Photo by ${item.owner.name}'"
+            >
+        </figure>
+        <router-link
+        class="photo_overlay"
+        :to="'/photos/${item.id}'"
+        :title="'View the photo by ${item.owner.name}'"
+        >
+        <div class="photo__controls">
+            <button
+                class="photo__action photo__action--like"
+                title="Like photo"
+            >
+                <i class="icon ion-md-heart"></i>12
+            </button>
+        </div>
+        </router-link >
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
