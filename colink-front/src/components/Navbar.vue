17b1a7037b800deb0ebc244df141c5f89059ffdf
nf-mugicha <natsuki.fujimoto.kk@gmail.com>
2019-06-19 18:10:17 +0900

    nav for responsive

--- colink-front/src/components/Navbar.vue
+++ colink-front/src/components/Navbar.vue
@@ -1,12 +1,29 @@
 <template>
     <nav class="nav-wrapper">
-        <router-link class="left brand-logo" to="/">
+        <router-link class="brand-logo" to="/">
             Vuesplash
         </router-link>
-        <!-- <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a> -->
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
             <li class="navbar__item">
+                <router-link class="navbar__item" to="/page">
                 <a class="waves-effect waves-light btn">Create Links page</a>
+                </router-link>
+            </li>
+            <li class="navbar__item">
+                username
+            </li>
+            <li class="navbar__item">
+                <RouterLink class="button button--link" to="/login">
+                    Login / Register
+                </RouterLink>
+            </li>
+        </ul>
+        <ul class="sidenav" id="mobile-demo">
+            <li class="navbar__item">
+                <router-link class="navbar__item" to="/page">
+                <a class="waves-effect waves-light btn">Create Links page</a>
+                </router-link>
             </li>
             <li class="navbar__item">
                 username
