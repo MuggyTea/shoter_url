68097058f4f1cf302cdcfd66ffaf4804eca922fb
nf-mugicha <natsuki.fujimoto.kk@gmail.com>
2019-06-20 20:19:54 +0900

    bug fix about connect db

--- colink-front/src/components/pages/OneLink.vue
+++ colink-front/src/components/pages/OneLink.vue
@@ -1,30 +1,31 @@
 <template>
     <v-list three-line>
         <template v-for="(link_page, index) in link_pages">
-            {{ link_page }}
             {{ index }}
+            {{ link_page.link_url }}
+            {{ link_page.link_name }}
         </template>
     </v-list>
 </template>
 
 <script>
-import { db } from './../../plugins/firebase';
+import { db } from './../../plugins/firebase'
 
 export default {
-    name: "OneLink",
-    props:{
-        item: {
-            type: Object,
-            required: true
-        }
-    },
-    data: () => ({
-        link_pages: []
-    }),
-    firestore() {
-        return {
-            link_pages: db.collection('LinkPage')
-        }
+  name: 'OneLink',
+  props: {
+    item: {
+      type: Object,
+      required: true
     }
+  },
+  data: () => ({
+    link_pages: []
+  }),
+  firestore () {
+    return {
+      link_pages: db.collection('LinkPage')
+    }
+  }
 }
 </script>
