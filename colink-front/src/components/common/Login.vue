ff943abd01262755cf84010d52a024b3e78b46ee
nf-mugicha <natsuki.fujimoto.kk@gmail.com>
2019-07-25 19:16:32 +0900

    bug fix

--- colink-front/src/components/common/Login.vue
+++ colink-front/src/components/common/Login.vue
@@ -1,31 +1,3 @@
-<!--
-<template>
-    <div class="container--small">
-        <ul class="tab">
-            <li
-                class="tab__item"
-                :class="{ 'tab__item--active': tab == 1 }"
-                @click="tab = 1"
-            >
-            Login
-            </li>
-            <li
-                class="tab__item"
-                :class="{ 'tab__item--active': tab === 2 }"
-                @click="tab = 2"
-            >
-            Register
-            </li>
-        </ul>
-        <div class="panel" v-show="tab === 1">
-            Login Form
-        </div>
-        <div class="panel" v-show="tab === 2">
-            Register Form
-        </div>
-    </div>
-</template>
--->
 <template>
   <div class="login__auth">
     <template v-if="isAuth && userName && userPic">
@@ -35,20 +7,14 @@
       <p class="header__user-name">
         {{ userName }}
         {{ user }}
-        {{ userInfo }}
       </p>
     </template>
     <div class="isAuthButtonText">
       <button v-if="isAuth" class="header__auth-button" @click="signOut">Sign-Out</button>
       <button v-else class="header__auth-button" @click="signIn">Sign-in with Twitter</button>
     </div>
-    <!--
-        <router-view
-            :isAuth="isAuth"
-            :userName="userName"
-            :userPic="userPic"
-    ></router-view>-->
   </div>
+  <!-- <router-view :isAuth="isAuth" :userName="userName" :userPic="userPic"></router-view> -->
 </template>
 
 <script>
@@ -75,8 +41,6 @@ export default {
   mounted: function () {
     firebase.auth().onAuthStateChanged(
       user => {
-        console.log('ログイン確認')
-        console.log(this.user)
         this.user = user || {}
         this.isAuth = !!user
         this.userName = user
@@ -90,15 +54,8 @@ export default {
         this.userUid = user
           ? this.user.uid : null
       })
-    console.log('ユーザー確認')
-    this.firestore()
-    console.log(this.userInfo)
   },
   methods: {
-    signInandRegistar: function () {
-      this.signIn()
-      this.RegistToDB()
-    },
     signIn: function () {
       const provider = new firebase.auth.TwitterAuthProvider()
       firebase.auth().signInWithPopup(provider)
